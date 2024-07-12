e69b0243add77b8bf8813c40b3b262aed958e9d7
Alejandro Rojas <alejandro@h2d.dev>
2024-07-12 13:17:48 -0400

    axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,9 +7,9 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-// const api = axios.create({ baseURL: 'https://paraguerobackend.onrender.com' })
+const api = axios.create({ baseURL: 'https://paraguerobackend.onrender.com' })
 
-const api = axios.create({ baseURL: 'http://localhost:5001' })
+// const api = axios.create({ baseURL: 'http://localhost:5001' })
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
